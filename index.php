807fd3ac3ed332c9c12cf63ad2a610f55e2dffe7
Payraw Salih <payrawsa@buffalo.edu>
2018-06-13 17:56:44 -0400

    update to include database name

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
   $servername = "tethys.cse.buffalo.edu";
   $username = "payrawsa";
   $password = "Graduates";
-
+  $dbname= "test";
   // Create connection
-  $conn = new mysqli($servername, $username, $password);
+  $conn = new mysqli($servername, $username, $password,$dbname);
 
   // Check connection
   if ($conn->connect_error) {
