78749e4db234657a506ba031b38461396862cfc2
Payraw Salih <payrawsa@buffalo.edu>
2018-06-13 17:55:48 -0400

    minor bug fixes

--- test.php
+++ test.php
@@ -1,18 +1,27 @@
 <html>
-require_once('sql_connection.php');
+
+<?php
+require_once('index1.php');
 $courses = $conn->query('select * from Employee');
 ?>
-<select name="course" id= "course-list">
+
+<select>
 <option> Select Course:</option>
 
 
 <?php
 if($courses->num_rows > 0){
         //outputting the courses
-        while($row = $courses->fetch_assoc()){  ?>
+        while($row = $courses->fetch_assoc()){
+?>
 
-        <option> <?php echo $row["cid"]; ?></option>
-<?php   }
+        <option>
+                <?php
+                echo $row["cid"];
+                ?>
+        </option>
+<?php
+                        }
 }
 
 ?>
