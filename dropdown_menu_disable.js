8adfc7a1fb102a172e7b20b972472ae3a1143c64
Payraw Salih <payrawsa@buffalo.edu>
2018-06-14 18:48:39 -0400

    JS functions to disable dropdown menus

---
+++ dropdown_menu_disable.js
@@ -0,0 +1,10 @@
+<script>
+function on() {
+  document.getElementById("TA_List").disabled = true;
+}
+</script>
+<script>
+function off() {
+  document.getElementById("TA_List").disabled = false;
+}
+</script>
