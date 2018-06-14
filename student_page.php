f8d8beaba782e34bacd66fa7c24c12153c8f8a20
Payraw Salih <payrawsa@buffalo.edu>
2018-06-14 18:47:39 -0400

    Functions used on dropdown menus to disable dropdown
    Dropdown menu will not be enabled until previous menu options have been selected. This makes the second dropdown menu dependent on the first one and the third on the second etc...

--- student_page.php
+++ student_page.php
@@ -1,81 +1,80 @@
 <html>
 
- <head>
-  <title>Students Forum</title>
-  
+<head>
+  <title>TA Feedback</title>
+
   <link href="main.css" rel="stylesheet" type="text/css" />
+  <script src="dropdown_menu_disable.js"></script>
 
 
- </head>
+</head>
 
+<div class="ublogo">
+  <img src="ub-logo.png" />
+</div>
 
-   <div class="ublogo">
-      <img src="ub-logo.png" />
-   </div>
-   <!-- <button type="button">Professor Log In</button> -->
-   <br>
-   
-   <?php
-   require_once('index1.php');
-   $courses = $conn->query('select * from Employee');
-   ?>
-
-   <select id="course-list">
-   <option> Select Course:</option>
-
-
-   <?php
-   if($courses->num_rows > 0){
-        //outputting the courses
-        while($row = $courses->fetch_assoc()){
-?>
-        <option>
-                <?php
-                echo $row["cid"];
-                ?>
-        </option>
-<?php
-}
-}
-?>
-</select>   
-   <br>
 <br>
-<select id="course-list">
-   <option> Select TA:</option>
 
-
-   <?php
-mysqli_data_seek($courses,0);
-   if($courses->num_rows > 0){
-        //outputting the courses
-        while($row = $courses->fetch_assoc()){
-?>
-        <option>
-                <?php
-                echo $row["TA"];
-                ?>
-        </option>
 <?php
-}
-}
+require_once('index1.php');
+$courses = $conn->query('select * from Employee');
 ?>
+
+<select id="course-list">
+  <option> Select Course:</option>
+
+
+  <?php
+  if($courses->num_rows > 0){
+    //outputting the courses
+    while($row = $courses->fetch_assoc()){
+      ?>
+      <option>
+        <?php
+        echo $row["cid"];
+        ?>
+      </option>
+      <?php
+    }
+  }
+  ?>
 </select>
 
-   <!-- <h3 class="twoption">Select the TA:</h3> -->
-   <!-- Creating drop-down for the TA's of the courses -->
-   
-   <br>
+<br>
+<select id="TA_List" <?php if (1==0){?>"javascript:off()"<?php; } else {?> "javascript:off()" <?php ;}?>>
 
-   <h5>Rate the TA:</h5>
-   <!-- Text box for submitting feedback-->
-   <span>How satisfied are you with the TA? </span>
-   <div class="rating">
-<span>&#9734; </span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>
+
+  <option> Select TA:</option>
+
+
+  <?php
+  mysqli_data_seek($courses,0);
+  if($courses->num_rows > 0){
+    //outputting the courses
+    while($row = $courses->fetch_assoc()){
+      ?>
+      <option>
+        <?php
+        echo $row["TA"];
+        ?>
+      </option>
+      <?php
+    }
+  }
+  ?>
+</select>
+
+<br>
+
+<h5>Rate the TA:</h5>
+<!-- Text box for submitting feedback-->
+<span>How satisfied are you with the TA? </span>
+<div class="rating">
+  <span>&#9734; </span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>
 </div>
-   <textarea class="feedbackbox" type='text' name="textbox1" size="40" maxlength="100" rows="5" cols="40"></textarea>
-   <br>
-   <button type="button">Submit Feedback</button>
+<textarea class="feedbackbox" type='text' name="textbox1" size="40" maxlength="100" rows="5" cols="40"></textarea>
+<br><br>
+<button type="button">Submit Feedback</button>
 
-  
+
 </html>
