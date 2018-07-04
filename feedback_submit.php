57e5321c1ecf1d765c2bac09d57483fc3a833930
namithaps90 <32022168+namithaps90@users.noreply.github.com>
2018-07-03 23:46:13 -0400

    Feedback message updated with appropriate style, font and background

--- feedback_submit.php
+++ feedback_submit.php
@@ -1,12 +1,25 @@
 <html>
 <head>
-	
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link href="main.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link rel="stylesheet" href="form.css" >
 </head>
 <body>
 	
  	<?php
- 	require_once('sql_connection.php');
- 		
+ 	require_once('sql_connection.php');?>
+
+ 	<div class="container">
+    <div class="imagebg"></div>
+    <div class="row " style="margin-top: 50px">
+      <div >
+        <div class="col-sm-12 form-group">
+
+ 	<?php	
  	//prepare statement to prevent sql injection
  	$stmt = $conn->prepare("INSERT INTO Feedback_Table (cid,TA_ubit,feedback,TA_description,student_email,student_name,time_stamp) VALUES (?,?,?,?,?,?,?)");
  	$stmt -> bind_param("sssssss",$Course_ID,$UBIT,$Feedback,$TA_desc,$Stu_email,$Stu_name,$Time_stamp);
@@ -22,9 +35,9 @@
     //executing the query 	
  	$stmt->execute();
 if ($stmt->affected_rows === 0) {
-    echo "Your feedback has not been recorded. Please submit again.";
+    echo "<font color='white' size='50' face='Arial'><center>Your feedback has not been recorded. Please submit again.</center>";
 } else {
-    echo "Your feedback has been submitted successfully. Thank You!";
+    echo "<font color='white' size='50' face='Arial'><center>Your feedback has been submitted successfully. Thank You!</center>";
 }
 $stmt->close();
 $conn->close();
